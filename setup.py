ef91861cc09fd0f974e3045b5ae356ea00e1fc45
Antonio Almudevar Atienza <almudevar@hermes2.i3a.es>
2023-09-22 16:04:14 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(  name='variational_classifier',
+        version='1.0',
+        description='Variational Bayes Classifier',
+        author='Antonio Almudevar',
+        author_email='almudevar@unizar.es',
+        packages=['src'],
+    )
